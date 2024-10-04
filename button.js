accd6d129dbdffca63c3a4ae134b7afc15d143f7
SWETHA S <149590877+swetha-306@users.noreply.github.com>
2024-10-04 14:24:26 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,22 @@
+function red(){
+    document.querySelector('#rd').style.backgroundColor="red";
+    document.querySelector('body').style.backgroundColor="red";
+}
+
+function blue(){
+    document.querySelector('#bl').style.backgroundColor="blue";
+    document.querySelector('body').style.backgroundColor="blue";
+}
+
+function Green(){
+    document.getElementById("gn").style.backgroundColor="green";
+    document.querySelector('body').style.backgroundColor="green";
+
+}
+function defaul(){
+    document.getElementById("gn").style.backgroundColor="";
+    document.getElementById("rd").style.backgroundColor="";
+    document.getElementById("dft").style.backgroundColor="";
+    document.getElementById("bl").style.backgroundColor="";
+    document.querySelector('body').style.backgroundColor="";
+}
